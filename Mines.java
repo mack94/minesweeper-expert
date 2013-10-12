5e9f7df4b2e289187afd167d1d38185382c1706b
publicarray <s_mail_s@yahoo.com>
2013-10-12 14:37:09 +1000

    added menu bar

--- Mines.java
+++ Mines.java
@@ -2,17 +2,19 @@ package mines;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Mines extends JFrame {
 
     private final int WIDTH = 250;
-    private final int HEIGHT = 290;
+    private final int HEIGHT = 300;
 
     private JLabel statusbar;
-    
+    private JMenuBar menuBar;
+    private JMenu file, view;
+    private JMenuItem save, load, exit;
+
     public Mines() {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,15 +22,32 @@ public class Mines extends JFrame {
         setLocationRelativeTo(null);
         setTitle("Minesweeper");
 
+        menuBar = new JMenuBar();
+
+        file = new JMenu("File");
+        save = new JMenuItem("Save");
+        load = new JMenuItem("Load");
+        exit = new JMenuItem("Exit");
+        file.add(save);
+        file.add(load);
+        file.add(exit);
+
+        view = new JMenu("View");
+        menuBar.add(file);
+        menuBar.add(view);
+        
         statusbar = new JLabel("");
         add(statusbar, BorderLayout.SOUTH);
+        add(menuBar, BorderLayout.NORTH);
 
-        add(new Board(statusbar));
+        // creates new insence of board
+        add(new Board(statusbar)); 
 
         setResizable(false);
         setVisible(true);
     }
     
+    // creates new instance on mines
     public static void main(String[] args) {
         new Mines();
     }
