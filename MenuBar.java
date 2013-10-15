9d5dafc28e9dc9e7446f581139743248f04fa9af
Jared Rankin <bigbluecloud@hotmail.com>
2013-10-15 11:52:08 +1000

    Restructure of the MenuBar.java file

--- MenuBar.java
+++ MenuBar.java
@@ -6,43 +6,54 @@ import java.awt.event.*;
 public class MenuBar extends JMenuBar
 {
   private JMenuBar menuBar = new JMenuBar();
-  private JMenu file, view, help;
-  private JMenuItem save, load, exit, newGame, beginner, intermediate, expert, custom, pause, resolve;
+  private JMenu fileMenu, viewMenu, helpMenu;
+  private JMenuItem saveItem, loadItem, exitItem, newGameItem,
+    beginnerItem, intermediateItem, expertItem, customItem, pauseItem, resolveItem;
   
   public MenuBar()
   {
-    //Create menu items to add to File
-    file = new JMenu("File");
-    save = new JMenuItem("Save");
-    load = new JMenuItem("Load");
-    exit = new JMenuItem("Exit");
-    file.add(save);
-    file.add(load);
-    file.add(exit);
-
-    //Create menu items to add to View
-    view = new JMenu("Game");
-    newGame = new JMenuItem("New Game");
-    beginner = new JMenuItem("Beginner");
-    intermediate = new JMenuItem("Intermediate");
-    expert = new JMenuItem("Expert");
-    custom = new JMenuItem("Custom...");
-    pause = new JMenuItem("Pause");
-    resolve = new JMenuItem("Resolve");
-    view.add(newGame);
-    view.add(beginner);
-    view.add(intermediate);
-    view.add(expert);
-    view.add(custom);
-    view.add(pause);
-    view.add(resolve);
+    buildFileMenu();
+    buildGameMenu();
       
     //Add Help to the JMenu
-    help = new JMenu("Help");
+    helpMenu = new JMenu("Help");
 
     //Add File, View and Help to the JMenuBar
-    menuBar.add(file);
-    menuBar.add(view);
-    menuBar.add(help);
+    menuBar.add(fileMenu);
+    menuBar.add(viewMenu);
+    menuBar.add(helpMenu);
+  }
+  
+  private void buildFileMenu()
+  {
+    //Create menu items to add to File
+    fileMenu = new JMenu("File");
+    saveItem = new JMenuItem("Save");
+    loadItem = new JMenuItem("Load");
+    exitItem = new JMenuItem("Exit");
+    fileMenu.add(saveItem);
+    fileMenu.add(loadItem);
+    fileMenu.add(exitItem);
+  }
+  
+  private void buildGameMenu()
+  {
+    //Create menu items to add to View
+    viewMenu = new JMenu("Game");
+    newGameItem = new JMenuItem("New Game");
+    beginnerItem = new JMenuItem("Beginner");
+    intermediateItem = new JMenuItem("Intermediate");
+    expertItem = new JMenuItem("Expert");
+    customItem = new JMenuItem("Custom...");
+    pauseItem = new JMenuItem("Pause");
+    resolveItem = new JMenuItem("Resolve");
+    
+    viewMenu.add(newGameItem);
+    viewMenu.add(beginnerItem);
+    viewMenu.add(intermediateItem);
+    viewMenu.add(expertItem);
+    viewMenu.add(customItem);
+    viewMenu.add(pauseItem);
+    viewMenu.add(resolveItem);
   }
 }
